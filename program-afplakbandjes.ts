cdbb3968a0ee94328630150653d621b9f54004b0
gallejens <galle.jens@telenet.be>
2023-10-26 14:52:15 +0200

    feat(afplakbandjes): add netto calculation

--- program-afplakbandjes.ts
+++ program-afplakbandjes.ts
@@ -3,21 +3,45 @@ const SETTINGS = {
   WORKSHEET_NAME: 'Afplakbandjes TEST', // naam die het nieuwe werkblad krijgt
   OVERMAAT: 50, // overmaat in mm (aan 1 kant, dus wordt 2x toegevoegd in berekening)
   VERLIES: 0.3, // verlies (0.3 = 30%)
+  IGNORE: [
+    {
+      equals: 'Niet afplakken',
+    },
+    {
+      contains: 'Verstek',
+    },
+    {
+      contains: 'Overmaat',
+    },
+  ],
 };
 
 const COLUMNS = {
-  length: { idx: 0, unique: true },
-  width: { idx: 1, unique: true },
-  amount: { idx: 2, unique: false },
-  material: { idx: 3, unique: true },
-  rotation: { idx: 4, unique: true },
-  label: { idx: 5, unique: false },
-  id: { idx: 17, unique: false },
+  length: 0,
+  width: 1,
+  material: 3,
+  side_a: 6,
+  side_b: 7,
+  side_c: 8,
+  side_d: 9,
 };
 
+type ColumnKey = keyof typeof COLUMNS;
+
+const SIDE_TO_INCREASE_COLUMN: [ColumnKey, ColumnKey][] = [
+  ['side_a', 'length'],
+  ['side_b', 'width'],
+  ['side_c', 'length'],
+  ['side_d', 'width'],
+];
+
 const TEMPLATE_DATA: ExcelCell[][] = [
-  ['Materiaal', 'NETTO Lengte', 'Bruto lengte incl. verlies'],
-  ['Test materiaal', 60, 100],
+  [
+    'Materiaal',
+    'Afplakmateriaal',
+    'NETTO Lengte',
+    'Bruto lengte incl. verlies',
+  ],
 ];
 
 // Types
@@ -31,14 +55,51 @@ function main(workbook: ExcelScript.Workbook) {
     ...dataRange.getValues().map((r: ExcelCell[]) => [...r.map(i => i)]),
   ];
 
+  // Remove existing worksheet with name
+  const existingWorksheet = workbook.getWorksheet(SETTINGS.WORKSHEET_NAME);
+  if (existingWorksheet) {
+    existingWorksheet.delete();
+  }
+
+  // k: plaatmateriaal, v: (k: afplakmateriaal, v: lengte)
+  const grouppedValues = new Map<string, Map<string, number>>();
+
+  // calculate total length for each material and side
+  for (const rowValues of values) {
+    const material = String(rowValues[COLUMNS.material]);
+    const lengthsForMaterial =
+      grouppedValues.get(material) ?? new Map<string, number>();
+
+    for (const [side, increaseColumn] of SIDE_TO_INCREASE_COLUMN) {
+      const sideMaterial = String(rowValues[COLUMNS[side]]);
+      if (shouldIgnoreMaterial(sideMaterial)) continue;
+
+      const increase = Number(rowValues[COLUMNS[increaseColumn]]);
+      if (Number.isNaN(increase)) {
+        throw new Error(`${increaseColumn} is not a number`);
+      }
+
+      const existingLength = lengthsForMaterial.get(sideMaterial) ?? 0;
+      lengthsForMaterial.set(sideMaterial, existingLength + increase);
+    }
+
+    grouppedValues.set(material, lengthsForMaterial);
+  }
+
   // Copy template data
   const data = [...TEMPLATE_DATA.map(r => [...r])];
+
+  for (const [material, lengths] of Array.from(grouppedValues)) {
+    for (const [sideMaterial, length] of Array.from(lengths)) {
+      data.push([material, sideMaterial, length, 0]);
+    }
+  }
 
   const worksheet = workbook.addWorksheet();
   try {
     worksheet.setName(SETTINGS.WORKSHEET_NAME);
   } catch (e) {
-    console.log(`Failed to set worksheet for afplakbandjes, duplicate name?`);
+    console.log(`Failed to set worksheet for afplakbandjes`);
   }
 
   const columnCount = Math.max(...data.map(r => r.length));
@@ -46,4 +107,21 @@ function main(workbook: ExcelScript.Workbook) {
   range.setValues(data);
 
   range.getFormat().autofitColumns();
+}
+
+function shouldIgnoreMaterial(material: string) {
+  const lowercaseMaterial = material.toLocaleLowerCase();
+  for (const req of SETTINGS.IGNORE) {
+    if (req.equals) {
+      if (lowercaseMaterial === req.equals.toLocaleLowerCase()) return true;
+    } else if (req.contains) {
+      if (lowercaseMaterial.includes(req.contains.toLocaleLowerCase()))
+        return true;
+    } else {
+      throw new Error(
+        `Unknown ignore requirement: ${Object.keys(req).join(', ')}`
+      );
+    }
+  }
+  return false;
 }
